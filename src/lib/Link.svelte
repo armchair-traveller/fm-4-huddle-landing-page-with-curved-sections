1fc198260c1d3c58dc9d95f4a439e3b66b47cbd1
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-06-01 14:27:47 -0700

    anchor component

---
+++ src/lib/Link.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let variant = "filled";
+  let className = "";
+  export { className as class };
+
+  let styles = variant == "outline" ? "" : "";
+</script>
+
+<a href="##" class="text-white no-underline {className}">
+  <slot />
+</a>
