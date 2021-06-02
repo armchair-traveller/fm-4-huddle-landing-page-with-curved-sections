bfa5172afd0492d8065659c5d774cb9eae0c00e3
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-06-02 15:10:00 -0700

    add markup, set font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,68 @@
 <script>
   import Attribution from "$lib/Attribution.svelte";
+  import Link from "$lib/Link.svelte";
 </script>
 
 <svelte:head>
-  <!-- TODO: Title -->
+  Frontend Mentor | Huddle landing page with curved sections
 </svelte:head>
 
 <main>
   <section>
-    <h1>TODO: Add initial markup in src/routes/index.svelte</h1>
+    <Link>Try it free</Link>
+
+    <h1>Build The Community Your Fans Will Love</h1>
+
+    <p>
+      Huddle re-imagines the way we build communities. You have a voice, but so
+      does your audience. Create connections with your users as you engage in
+      genuine discussion.
+    </p>
+
+    <Link>Get Started For Free</Link>
+
+    <h2>1.4k+</h2>
+    <p>Communities Formed</p>
+
+    <h2>2.7m+</h2>
+    <p>Messages Sent</p>
+
+    <h2>Grow Together</h2>
+    <p>
+      Generate meaningful discussions with your audience and build a strong,
+      loyal community. Think of the insightful conversations you miss out on
+      with a feedback form.
+    </p>
+
+    <h2>Flowing Conversations</h2>
+    <p>
+      You wouldn't paginate a conversation in real life, so why do it online?
+      Our threads have just-in-time loading for a more natural flow.
+    </p>
+
+    <h2>Your Users</h2>
+    <p>
+      It takes no time at all to integrate Huddle with your app's authentication
+      solution. This means, once signed in to your app, your users can start
+      chatting immediately.
+    </p>
+
+    <h2>Ready To Build Your Community?</h2>
+    <Link>Get Started For Free</Link>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla
+      quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+    </p>
+    <h2>Newsletter</h2>
+    <p>
+      To recieve tips on how to grow your community, sign up to our weekly
+      newsletter. We’ll never send you spam or pass on your email address
+    </p>
+    <Link>Subscribe</Link>
+
+    <p>Phone: +1-543-123-4567</p>
+    <p>example@huddle.com</p>
   </section>
 </main>
 
@@ -17,9 +71,9 @@
 <style>
   main {
     /* font-size: 14px; */
-    /* font-family: , system-ui */
+    font-family: "Open Sans", system-ui;
     /* @apply bg-main-400 min-h-full; */
-    /* @apply text-main-100; */
+    @apply text-neutral-100;
   }
 
   /* TODO: Add your design breakpoints and additional details here, for reference while styling. */
