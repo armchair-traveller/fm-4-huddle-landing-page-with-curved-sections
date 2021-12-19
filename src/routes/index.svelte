5ba4e63fdf90d1932edebacda9a149828db1fb8b
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-12-19 15:10:42 -0800

    mobile main sections + init footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,22 +6,21 @@ import Attribution from '$lib/Attribution.svelte'
   <title>Frontend Mentor | Huddle landing page with curved sections</title>
 </svelte:head>
 
-<main>
-  <header class="flex justify-between p-3">
-    <img class="w-28 object-contain" src="/images/logo.svg" alt="logo" />
-    <a class="border-primary-100 border rounded-full text-primary-100 px-5 py-1 text-sm" href="/">Try it free</a>
-  </header>
-
+<header class="flex justify-between p-3 text-neutral-100">
+  <img class="w-28 object-contain" src="/images/logo.svg" alt="logo" />
+  <a class="border-primary-100 border rounded-full text-primary-100 px-5 py-1 text-sm" href="/">Try it free</a>
+</header>
+<main class="text-neutral-100">
   <section class="flex flex-col">
     <h1 class="text-center font-bold text-3xl mt-20">Build The Community<br /> Your Fans Will Love</h1>
 
-    <p class="px-3 my-8 text-center font-semibold">
+    <p class="px-4 my-7 text-center">
       Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections
       with your users as you engage in genuine discussion.
     </p>
 
     <a
-      class="mt-4 text-center py-3 px-12 rounded-full bg-primary-100 text-primary-400 font-semibold text-sm self-center bg"
+      class="mt-4 text-center py-3 px-12 rounded-full bg-primary-100 text-primary-400 font-semibold text-sm self-center"
       href="/">Get Started For Free</a
     >
   </section>
@@ -46,12 +45,12 @@ import Attribution from '$lib/Attribution.svelte'
   <section class="bg-neutral-200 flex flex-col items-center">
     <img class="w-4/5 mt-20" src="/images/illustration-grow-together.svg" alt="grow together illustration" />
     <h2 class="mt-20 font-bold text-2xl">Grow Together</h2>
-    <p class="mt-4 mb-12 text-center w-11/12">
+    <p class="mt-4 mb-12 text-center w-10/12">
       Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful
       conversations you miss out on with a feedback form.
     </p>
   </section>
-  <div class="[ bg-section-top-2 ] h-[103px] rotate-180 bg-no-repeat" />
+  <div class="[ bg-section-bottom-1 ] h-[103px] bg-no-repeat" />
 
   <section class="flex flex-col items-center">
     <img
@@ -60,36 +59,57 @@ import Attribution from '$lib/Attribution.svelte'
       alt="flowing conversation illustration"
     />
     <h2 class="mt-20 font-bold text-2xl">Flowing Conversations</h2>
-    <p class="mt-4 mb-12 text-center w-11/12">
+    <p class="mt-4 mb-12 text-center w-10/12">
       You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for
       a more natural flow.
     </p>
   </section>
 
-  <img class="w-4/5 mt-20" src="/images/illustration-your-users.svg" alt="your users illustration" />
-  <h2>Your Users</h2>
-  <p>
-    It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to
-    your app, your users can start chatting immediately.
-  </p>
+  <div class="[ bg-section-top-2 ] h-[103px] mt-20 bg-no-repeat" />
+  <section class="bg-neutral-200 flex flex-col items-center">
+    <img class="w-4/5 mt-8" src="/images/illustration-your-users.svg" alt="your users illustration" />
+    <h2 class="mt-20 font-bold text-2xl">Your Users</h2>
+    <p class="mt-4 mb-32 text-center w-10/12">
+      It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to
+      your app, your users can start chatting immediately.
+    </p>
+  </section>
+  <div class="[ bg-section-bottom-2 ] h-[103px] bg-no-repeat" />
 
-  <h2>Ready To Build Your Community?</h2>
-  <a href="/">Get Started For Free</a>
-
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia vestibulum a, ultrices
-    quis sem.
-  </p>
-  <h2>Newsletter</h2>
-  <p>
-    To recieve tips on how to grow your community, sign up to our weekly newsletter. We’ll never send you spam or pass
-    on your email address
-  </p>
-  <a href="/">Subscribe</a>
-
-  <p>Phone: +1-543-123-4567</p>
-  <p>example@huddle.com</p>
+  <section class="flex flex-col items-center mt-20">
+    <h2 class="text-2xl font-bold text-center">Ready To Build Your Community?</h2>
+    <a
+      class="mt-10 text-center py-3 px-12 rounded-full bg-primary-100 text-primary-400 font-semibold text-sm self-center"
+      href="/">Get Started For Free</a
+    >
+  </section>
 </main>
+<div class="[ bg-footer-top ] h-[53px] bg-no-repeat mt-60" />
+<footer class="bg-neutral-100 text-neutral-200">
+  <section class="flex flex-col p-4">
+    <h2 class="text-xl font-bold mt-12 uppercase">Newsletter</h2>
+    <p class="mt-4">
+      To recieve tips on how to grow your community, sign up to our weekly newsletter. We’ll never send you spam or pass
+      on your email address
+    </p>
+    <input class="mt-7 p-3 text-neutral-100 rounded-md" type="text" name="" id="" />
+    <a
+      class="w-max mt-4 text-center py-3 px-12 rounded-md bg-primary-100 text-primary-400 font-semibold text-lg self-end"
+      href="/">Subscribe</a
+    >
+  </section>
+
+  <section>
+    <img class="w-48 object-contain mt-28" src="/images/logo-white.svg" alt="white logo" />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia vestibulum a,
+      ultrices quis sem.
+    </p>
+    <p>Phone: +1-543-123-4567</p>
+    <p>example@huddle.com</p>
+    <!-- TODO: Social icon links -->
+  </section>
+</footer>
 
 <Attribution />
 
@@ -97,14 +117,21 @@ import Attribution from '$lib/Attribution.svelte'
 main {
   /* font-size: 14px; */
   font-family: 'Open Sans', system-ui;
-  /* @apply bg-main-400 min-h-full; */
-  @apply text-neutral-100;
 }
 .bg-section-top-1 {
   background-image: url('/images/bg-section-top-mobile-1.svg');
 }
 .bg-section-top-2 {
   background-image: url('/images/bg-section-top-mobile-2.svg');
+}
+.bg-section-bottom-1 {
+  background-image: url('/images/bg-section-bottom-mobile-1.svg');
+}
+.bg-section-bottom-2 {
+  background-image: url('/images/bg-section-bottom-mobile-2.svg');
+}
+.bg-footer-top {
+  background-image: url('/images/bg-footer-top-mobile.svg');
 }
 /* breakpoints: 375, 1440 */
 </style>
